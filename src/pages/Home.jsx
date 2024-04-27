0389ed4ba579df77e932d01d0e568a9a31aeb15d
hasanonik <jh8539615@gmail.com>
2024-04-27 12:39:36 +0600

    added Countries Section on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,36 @@ const Home = () => {
                 <div className="max-w-[1440px] px-5 mx-auto">
                     <Slider></Slider>
                 </div>
-                <div className='mt-10 mb-10 flex bg-[#f7f8fa]'>
+                <div className="max-w-[1440px] px-5 mx-auto my-20">
+                    <h1 className='text-4xl text-center font-semibold text-black mb-10'>Top Destinations</h1>
+                    <div className='flex justify-around flex-wrap gap-5'>
+                        <div className='text-center'>
+                            <div className='w-[200px] h-[200px] rounded-full bg-red-100 overflow-hidden country country-1'></div>
+                            <h4 className='mt-4 text-xl text-black font-medium'>Bangladesh</h4>
+                        </div>
+                        <div className='text-center'>
+                            <div className='w-[200px] h-[200px] rounded-full bg-red-100 overflow-hidden country country-2'></div>
+                            <h4 className='mt-4 text-xl text-black font-medium'>Thailand</h4>
+                        </div>
+                        <div className='text-center'>
+                            <div className='w-[200px] h-[200px] rounded-full bg-red-100 overflow-hidden country country-3'></div>
+                            <h4 className='mt-4 text-xl text-black font-medium'>Indonesia</h4>
+                        </div>
+                        <div className='text-center'>
+                            <div className='w-[200px] h-[200px] rounded-full bg-red-100 overflow-hidden country country-4'></div>
+                            <h4 className='mt-4 text-xl text-black font-medium'>Malaysia</h4>
+                        </div>
+                        <div className='text-center'>
+                            <div className='w-[200px] h-[200px] rounded-full bg-red-100 overflow-hidden country country-5'></div>
+                            <h4 className='mt-4 text-xl text-black font-medium'>Vietnam</h4>
+                        </div>
+                        <div className='text-center'>
+                            <div className='w-[200px] h-[200px] rounded-full bg-red-100 overflow-hidden country country-6'></div>
+                            <h4 className='mt-4 text-xl text-black font-medium'>Cambodia</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className='mb-10 flex bg-[#f7f8fa]'>
                     <div className='max-w-[1440px] mx-auto px-5 pt-24 pb-24 w-full'>
                         <div className='touristsSpot-container w-full'>
                             {
