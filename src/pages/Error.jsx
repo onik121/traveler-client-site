9104928f22fb089896976063b2e554adf3b7cf31
hasanonik <jh8539615@gmail.com>
2024-04-30 21:47:32 +0600

    added lottie react package

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,14 @@
 
 import { Link } from 'react-router-dom';
-import img from '../assets/404.png'
+import error from '../lottie.json'
+import Lottie from "lottie-react";
 
 const Error = () => {
     return (
         <div className='max-w-[1440px] mx-auto px-5 flex items-center justify-center h-screen'>
             <div className='text-center'>
                 <div className='flex justify-center'>
-                    <img src={img}></img>
+                    <Lottie className='max-w-[300px]' loop={true} animationData={error}></Lottie>
                 </div>
                 <h1 className='text-5xl text-black font-medium mt-4 mb-3'>Opps! Page Not Found</h1>
                 <Link to={'/'}><button className='go-back mt-5'>Go Back</button></Link>
